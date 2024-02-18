3262c8c997b6ce642680d1e9f960efd15b4df0b3
sasha <sashasami03@gmail.com>
2024-02-18 19:49:33 +0100

    moved to github workflow action

---
+++ src/BlazorApp/Models/Quote.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorApp.Models
+{
+    public class Quote
+    {
+        public string ImageName { get; set; }
+        public string Title { get; set; }
+        public string QuoteString { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+        public string Tag { get; set; }
+
+    }
+}
