4cb8b51e4470afcdad15f9e4465cb08778766ac4
sasha <sashasami03@gmail.com>
2024-02-20 20:10:53 +0100

    moving to fa-icons all together, even using them depending on what the quotes tag is

--- src/BlazorApp/Models/Quote.cs
+++ src/BlazorApp/Models/Quote.cs
@@ -1,5 +1,14 @@
 ﻿namespace BlazorApp.Models
 {
+    public enum TagType
+    {
+        MOVIE,
+        SONG,
+        TV_SERIES,
+        PERSONALITY,
+        STORY
+    }
+
     public class Quote
     {
         public string ImageName { get; set; }
@@ -7,7 +16,32 @@
         public string QuoteString { get; set; }
         public string Description { get; set; }
         public string Link { get; set; }
-        public string Tag { get; set; }
+        public string Tag {  get; set; }
 
+        public string GetTagIconName()
+        {
+            TagType? tagType = null;
+            try
+            {
+                tagType = Enum.Parse<TagType>(Tag.Replace(" ", "_"), true);
+            }
+            catch (Exception _) { }
+
+            switch (tagType)
+            {
+                case TagType.MOVIE:
+                    return "fa fa-film";
+                case TagType.TV_SERIES:
+                    return "fa fa-tv";
+                case TagType.SONG:
+                    return "fa fa-music";
+                case TagType.PERSONALITY:
+                    return "fa fa-user";
+                case TagType.STORY:
+                    return "fa fa-book";
+                default:
+                    return "";
+            }
+        }
     }
 }
